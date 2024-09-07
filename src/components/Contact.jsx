7ee4a16e769547554adb5cbe1ba9e1a3b2b98457
Mtechnocrat <ayush2429.be21@chitkara.edu.in>
2024-09-07 22:11:57 +0530

    Project complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,11 +13,11 @@ export const Contact = () => {
             <a href="#" className="border-b">{CONTACT.email}</a> */}
 
                 <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
-                    <motion.a href="https://www.linkedin.com/in/ayush-kumar-4b07b122b/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></motion.a>
-                    <a href="https://github.com/Mtechnocrat" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-                    <a href="https://x.com/MrGenixxx" target="_blank" rel="noopener noreferrer"><FaSquareTwitter /></a>
-                    <a href="https://www.instagram.com/aayush_1020/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-                    <a href="mailto:mr.genixx@gmail.com" target="_blank" rel="noopener noreferrer"><SiGmail/> </a>      
+                    <motion.a whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:1}} href="https://www.linkedin.com/in/ayush-kumar-4b07b122b/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></motion.a>
+                    <motion.a whileInView={{opacity:1,x:0}} initial={{opacity:0,x:-100}} transition={{duration:1}}  href="https://github.com/Mtechnocrat" target="_blank" rel="noopener noreferrer"><FaGithub /></motion.a>
+                    <motion.a whileInView={{opacity:1,y:0}} initial={{opacity:0,y:100}} transition={{duration:1}} href="https://x.com/MrGenixxx" target="_blank" rel="noopener noreferrer"><FaSquareTwitter /></motion.a>
+                    <motion.a whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:1}} href="https://www.instagram.com/aayush_1020/" target="_blank" rel="noopener noreferrer"><FaInstagram /></motion.a>
+                    <motion.a whileInView={{opacity:1,x:0}} initial={{opacity:0,x:100}} transition={{duration:1}} href="mailto:mr.genixx@gmail.com" target="_blank" rel="noopener noreferrer"><SiGmail/> </motion.a>      
                 </div>
             </div>
         </div>
